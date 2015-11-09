95d36139f270479c24a2967a0eceddb5e2acd578
User <user@Users-MacBook-Pro-10.local>
2015-11-09 19:32:59 +0000

    cancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     secret_path
   end
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, alert: "You can't access this page"
+  end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
